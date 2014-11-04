e336c0f631379a9e292cfd2f22466af528f42690
Gabriele <neothematrix@libero.it>
2014-11-04 13:58:14 +0100

    fixed powershell script
    Modificato script powershell per restituire correttamente il numero
    delle VM anche nei job di Replica
    sistemato anche il tipo di job in modo che ritorni "Backup" o "Replica"
    (prima ritornava item non supportato in caso di Replica).

--- zabbix_vbr_job.ps1
+++ zabbix_vbr_job.ps1
@@ -7,87 +7,89 @@
 #
 # RETURNED DATA:
 #
-#      Id                     : 5e6063aa-49b1-4515-9711-01e6d3083443
-#      Name                   : KHP-BCK-JOB-007 - MyJob
-#      IsScheduleEnabled      : True
+# Id : 5e6063aa-49b1-4515-9711-01e6d3083443
+# Name : KHP-BCK-JOB-007 - MyJob
+# IsScheduleEnabled : True
 #
 # USAGE:
-#   as a script:    C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe -command  "& C:\Zabbix\zabbix_vbr_job.ps1 <ITEM_TO_QUERY> <JOBID>"
-#   as an item:     vbr[<ITEM_TO_QUERY>,<JOBID>]
+# as a script: C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe -command "& C:\Zabbix\zabbix_vbr_job.ps1 <ITEM_TO_QUERY> <JOBID>"
+# as an item: vbr[<ITEM_TO_QUERY>,<JOBID>]
 #
 # Add to Zabbix Agent
-#   UserParameter=vbr[*],%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command "& C:\Zabbix\zabbix_vbr_job.ps1 $1 $2"
+# UserParameter=vbr[*],%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command "& C:\Zabbix\zabbix_vbr_job.ps1 $1 $2"
 #
-$version = "1.0.0"
-
+$version = "1.0.1"
 $ITEM = [string]$args[0]
 $ID = [string]$args[1]
-
 #* Load Veeam snapin
 Add-PsSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue
-
 # Query VEEAM for Job
 switch ($ITEM) {
-  "Discovery" {
-    # Open JSON object
-    $output =  "{`"data`":["
-      $query = Get-VBRJob | Select-Object Id,Name, IsScheduleEnabled
-      $count = $query | Measure-Object
-      $count = $count.count
-      foreach ($object in $query) {
-        $Id = [string]$object.Id
-        $Name = [string]$object.Name
-        $Schedule = [string]$object.IsScheduleEnabled
-        if ($count -eq 1) {
-          $output = $output + "{`"{#JOBID}`":`"$Id`",`"{#JOBNAME}`":`"$Name`",`"{#JOBSCHEDULED}`":`"$Schedule`"}"
-        } else {
-          $output = $output + "{`"{#JOBID}`":`"$Id`",`"{#JOBNAME}`":`"$Name`",`"{#JOBSCHEDULED}`":`"$Schedule`"},"
-        }
-        $count--
-    }
-    # Close JSON object
-    $output = $output + "]}"
-    Write-Host $output
-  }
-  "Status"  {
-  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
-    switch ([string]$query.GetLastResult()) {
-      "Failed" {
-        return "0"
-      }
-      "Warning" {
-        return "1"
-      }
-      default {
-        return "2"
-      }
-    }
-  }
-  "IncludedSize"{
-  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
-  [string]$query.Info.IncludedSize
-  }
-  "ExcludedSize"{
-  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
-  [string]$query.Info.ExcludedSize
-  }
-  "VmCount" {
-  $query = Get-VBRBackup | Where-Object {$_.JobId -like "*$ID*"}
-  [string]$query.VmCount
-  }
-  "Type" {
-  $query = Get-VBRBackup | Where-Object {$_.JobId -like "*$ID*"}
-  [string]$query.JobType
-  }
-  "RunningJob" {
-  $query = Get-VBRBackupSession | where { $_.isCompleted -eq $false }
-  if ($query) {
-	[string]$query.Count
-    } else {
-	0
-    }
-  }
-  default {
-      Write-Host "-- ERROR -- : Need an option to work !"
-  }
+	"Discovery" {
+# Open JSON object
+		$output = "{`"data`":["
+		$query = Get-VBRJob | Select-Object Id,Name, IsScheduleEnabled
+		$count = $query | Measure-Object
+		$count = $count.count
+		foreach ($object in $query) {
+			$Id = [string]$object.Id
+			$Name = [string]$object.Name
+			$Schedule = [string]$object.IsScheduleEnabled
+			if ($count -eq 1) {
+				$output = $output + "{`"{#JOBID}`":`"$Id`",`"{#JOBNAME}`":`"$Name`",`"{#JOBSCHEDULED}`":`"$Schedule`"}"
+			} else {
+				$output = $output + "{`"{#JOBID}`":`"$Id`",`"{#JOBNAME}`":`"$Name`",`"{#JOBSCHEDULED}`":`"$Schedule`"},"
+			}
+			$count--
+		}
+# Close JSON object
+		$output = $output + "]}"
+		Write-Host $output
+	}
+	"Status" {
+		$query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+		switch ([string]$query.GetLastResult()) {
+			"Failed" {
+				return "0"
+			}
+			"Warning" {
+				return "1"
+			}
+			default {
+				return "2"
+			}
+		}
+	}
+	"IncludedSize"{
+		$query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+		[string]$query.Info.IncludedSize
+	}
+	"ExcludedSize"{
+		$query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+		[string]$query.Info.ExcludedSize
+	}
+	"VmCount" {
+		$type = (Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}).JobType
+		switch($type) {
+			"Backup" { $query = Get-VBRBackup | Where-Object {$_.JobId -like "*$ID*"} }
+			"Replica" { $query = Get-VBRReplica | Where-Object {$_.JobId -like "*$ID*"} }
+			default { "ZBX_NOTSUPPORTED" }
+		}
+		[string]$query.VmCount
+	}
+	"Type" {
+		$query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+		[string]$query.JobType
+	}
+	"RunningJob" {
+		$query = Get-VBRBackupSession | where { $_.isCompleted -eq $false }
+		if ($query) {
+			[string]$query.Count
+		} else {
+			0
+		}
+	}
+	default {
+		Write-Host "-- ERROR -- : Need an option to work !"
+	}
 }
